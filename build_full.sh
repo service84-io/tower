342c021b19d30e55cfec3c2bcae2654acac66847
Tyler <Tyler@service84.io>
2025-08-11 09:01:54 -0400

    Use Boot

--- build_full.sh
+++ build_full.sh
@@ -3,6 +3,6 @@
 tower DSA.Vent.Tower.dbnf DSA::Vent::Tower::CPPGenerator
 tower DSA.Vent.Tower.dbnf DSA::Vent::Tower::CTCodeGeneratorBoot
 
-ctcode DSA.Vent.Tower.dbnf.ctcode s84::ctcode::CPPTranspiler
+ctcode DSA.Vent.Tower.dbnf.ctcode s84::ctcode::CPPTranspilerBoot
 
 $(dirname $0)/build_source.sh
